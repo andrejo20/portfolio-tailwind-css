eef73ac0331fb4e787d3ca93af5cb9840c5859b3
Anjo <andre@sqiva.com>
2023-02-07 11:53:33 +0700

    update dark mode + perbaikan fitur

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,14 +2,19 @@
 window.onscroll = function(){
     const header = document.querySelector('header');
     const fixedNav = header.offsetTop;
+    const toTop = document.querySelector('#toTop');
 
     if (window.pageYOffset > fixedNav)
     {
         header.classList.add('navbar-fixed');
+        toTop.classList.remove('hidden');
+        toTop.classList.add('flex');
     }
     else
     {
         header.classList.remove('navbar-fixed');
+        toTop.classList.add('hidden');
+        toTop.classList.remove('flex');
     }
 }
 
@@ -21,3 +26,40 @@ hamburger.addEventListener('click', function(){
     hamburger.classList.toggle('hamburger-active');
     navMenu.classList.toggle('hidden');
 });
+
+// Klik di luar hamburger
+window.addEventListener('click', function(e){
+    if (e.target != hamburger && e.target != navMenu)
+    {
+        hamburger.classList.remove('hamburger-active');
+        navMenu.classList.add('hidden');
+    }
+});
+
+// Dark Mode Toggle
+const darkToggle = document.querySelector('#dark-toggle');
+const html = document.querySelector('html');
+
+darkToggle.addEventListener('click', function(){
+    if (darkToggle.checked)
+    {
+        html.classList.add('dark');
+        localStorage.theme = 'dark';
+    }
+    else
+    {
+        html.classList.remove('dark');
+        localStorage.theme = 'light';
+    }
+});
+
+// // pindahkan posisi toggle sesuai mode
+// // On page load or when changing themes, best to add inline in `head` to avoid FOUC
+// if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) 
+// {
+//     darkToggle.checked = true;
+// } 
+// else
+// {
+//     darkToggle.checked = false;
+// }
